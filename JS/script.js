cacef25fbbbc75df1bc03911e6c36ea7d82ce737
Ricardo Tellez Nieto <a282106@uach.mx>
2015-11-08 18:21:02 -0700

    Esta es la practica derecho a examen con menu deslizable cabe mensionar que esta practica fue hecha en equipo

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+$("document").ready(function(){
+  $( "#prom1" ).click(function() {
+    $( "#art1" ).slideToggle( "slow", function() {
+    });
+  });
+  $( "#prom2" ).click(function() {
+    $( "#art2" ).slideToggle( "slow", function() {
+    });
+  });
+  $( "#prom3" ).click(function() {
+    $( "#art3" ).slideToggle( "slow", function() {
+    });
+  });
+});
